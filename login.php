35d7b8a43275a0f9414efa69f1398b101b5c038e
Izzul Afif <maz.izzul@gmail.com>
2014-04-02 15:05:45 +0700

    Revert "commit awal"
    This reverts commit f7f81573206a724627a34d9bf265fd2d52955bb6.

--- login.php
+++
@@ -1,3 +0,0 @@
-<form id="fm" method="post" action="auth.php">   	<div class="fitem">  		<label>Username :</label>  		<input autocomplete="off" id="hh" name="u" size="28" maxlength="18"/> 	</div>	<div class="fitem">  		<label>Password :</label>  		<input type="password" name="p" size="28" maxlength="50"/> 	</div>  
-</form><script type="text/javascript">$('input#hh').focus();
-</script>
